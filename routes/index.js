df80f079df01aa37ba8934eed28704fde12ac028
shine <shine@vip.qq.com>
2019-05-28 10:55:12 +0800

    Revert "mongo连接"
    This reverts commit 3b2ae973

--- routes/index.js
+++ routes/index.js
@@ -74,7 +74,6 @@ router.post('/app/login_action', function (req, res, next) {
         let MongoURI = require('mongo-uri');
         try {
             MongoURI.parse(dbstr);
-            console.log(dbstr);
             connPool.addConnection({connName: req.body.inputDB, connString: dbstr}, req.app, function (err, data) {
                 if (err) {
                     res.render('login', {
